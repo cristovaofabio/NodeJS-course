9c3ea8bf36e90b2acc0aac964ddb68fdb475b856
Fabio <cristovaofabio3@gmail.com>
2022-08-03 19:49:42 -0300

    feat: html pages

--- router/shop.js
+++ router/shop.js
@@ -1,10 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log('Middleware!');
-    res.send('<h1>Hello from Express home page</h1>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
