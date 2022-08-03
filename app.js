a92b7761ced6c4fac99b6b48eed7939627ca5d80
Fabio <cristovaofabio3@gmail.com>
2022-08-03 08:12:58 -0300

    feat: added middleware

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('First middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Second middleware!');
+    res.send('<h1>Hello from Express</h1>');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
