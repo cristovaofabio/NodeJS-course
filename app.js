1e8b5bc0186b45231b896ed9ca9222eb3fac622f
Fabio <cristovaofabio3@gmail.com>
2022-08-03 11:13:41 -0300

    feat: parsing incoming requests

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/add-product', (req, res, next) => {
-    res.send('<h1>Add product page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body.title);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
     console.log('Middleware!');
-    res.send('<h1>Hello from Express</h1>');
+    res.send('<h1>Hello from Express home page</h1>');
 });
 
 app.listen(3000);
