477b1b9dde846943cf3a8fe26c223712853326f7
Fabio <cristovaofabio3@gmail.com>
2022-08-03 08:43:40 -0300

    feat: diffent routes

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware!');
-    next();
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>Add product page</h1>');
 });
 
-app.use((req, res, next) => {
-    console.log('Second middleware!');
+app.use('/', (req, res, next) => {
+    console.log('Middleware!');
     res.send('<h1>Hello from Express</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
