80c39ad8d756ea7182331edab688de67c27864c7
Fabio <cristovaofabio3@gmail.com>
2022-08-04 15:58:45 -0300

    feat: sharing data

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./router/admin.js');
+const adminData = require('./router/admin.js');
 const shopRoutes = require('./router/shop.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname,'views', '404.html'))
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
 app.listen(3000);
