b3feb9bf181f150f4c0e5c46dcdebb3e4d4b26a6
Fabio <cristovaofabio3@gmail.com>
2022-08-05 19:48:41 -0300

    feat: pug template finished

--- router/admin.js
+++ router/admin.js
@@ -8,7 +8,7 @@ const products = [];
 
 // /admin/add-product
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add product' })
+    res.render('add-product', { pageTitle: 'Add product', path: '/admin/add-product' })
 });
 
 // /admin/add-product
