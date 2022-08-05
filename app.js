f074f24f8217086bd5fa7cb1c0facad6361a85d3
Fabio <cristovaofabio3@gmail.com>
2022-08-05 09:25:57 -0300

    feat: implementing pug

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./router/admin.js');
 const shopRoutes = require('./router/shop.js');
 
