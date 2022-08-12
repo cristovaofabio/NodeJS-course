f5ffcf8baf1679cf7c84dd89b2e8ca6fcad0f7b2
Fabio <cristovaofabio3@gmail.com>
2022-08-12 09:00:48 -0300

    feat: displaying cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,9 +33,21 @@ exports.getIndex = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your cart',
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const carProducts = [];
+            for (product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if (cartProductData) {
+                    carProducts.push({ productData: product, qty: cartProductData.qty });
+                }
+            }
+            res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your cart',
+                products: carProducts
+            });
+        });
     });
 }
 
