5670e86ed56920518723f4b5d8d902b4f150230c
Fabio <cristovaofabio3@gmail.com>
2022-08-22 10:18:56 -0300

    feat: associate cart to user

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('6300ff89bf46823b5dc7e570')
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => console.log(err));
