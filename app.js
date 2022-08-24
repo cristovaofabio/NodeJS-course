1769f8b8c2633fe473ab33fc6a7070c122abee92
Fabio <cristovaofabio3@gmail.com>
2022-08-24 10:05:40 -0300

    feat: saving data through mongoose

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -20,21 +20,21 @@ const shopRoutes = require('./router/shop.js');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById('6300ff89bf46823b5dc7e570')
-        .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//     User.findById('6300ff89bf46823b5dc7e570')
+//         .then(user => {
+//             req.user = new User(user.name, user.email, user.cart, user._id);
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.pageNotFound);
 
-mongoose.connect(`mongodb+srv://cristovaofabio:${process.env.MONGO_PASSWORD}@cluster0.sio0ccb.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://cristovaofabio:${process.env.MONGO_PASSWORD}@cluster0.sio0ccb.mongodb.net/shop?retryWrites=true&w=majority`)
     .then(result => {
         console.log('Connected to database!');
         app.listen(3000);
--- router/admin.js
+++ router/admin.js
@@ -6,14 +6,14 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// // /admin/add-product
-// router.get('/add-product', adminController.getAddProduct);
+// /admin/add-product
+router.get('/add-product', adminController.getAddProduct);
 
 // // /admin/products => GET
 // router.get('/products', adminController.getProducts);
 
-// // /admin/add-product
-// router.post('/add-product', adminController.postAddProduct);
+// /admin/add-product
+router.post('/add-product', adminController.postAddProduct);
 
 // router.get('/edit-product/:productId', adminController.getEditProduct);
 
