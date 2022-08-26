08666d144260201a6731b54a5670f4dbc718bde1
Fabio <cristovaofabio3@gmail.com>
2022-08-26 09:42:37 -0300

    feat: working with user model

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -20,14 +20,14 @@ const shopRoutes = require('./router/shop.js');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById('6300ff89bf46823b5dc7e570')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findById('6308bd809796725e508d28c9')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,6 +36,18 @@ app.use(errorController.pageNotFound);
 
 mongoose.connect(`mongodb+srv://cristovaofabio:${process.env.MONGO_PASSWORD}@cluster0.sio0ccb.mongodb.net/shop?retryWrites=true&w=majority`)
     .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    username: 'Tony',
+                    email: 'tony@email.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
         console.log('Connected to database!');
         app.listen(3000);
     })
