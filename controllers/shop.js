abcbccb9438b1ec0d1ced614763b01fb8b1d3fbd
Fabio <cristovaofabio3@gmail.com>
2022-08-27 15:53:28 -0300

    feat: storing orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
     Product.find()
@@ -68,33 +69,6 @@ exports.postCart = (req, res, next) => {
             res.redirect('/cart');
         })
         .catch(err => console.log(err));
-    // let fetchedCart;
-    // let newQuantity = 1;
-    // req.user
-    //     .getCart()
-    //     .then(cart => {
-    //         fetchedCart = cart;
-    //         return cart.getProducts({ where: { id: prodId } })
-    //     }).then(products => {
-    //         let product;
-    //         if (products.length > 0) product = products[0];
-    //         if (product) {
-    //             const oldQuantity = product.cartItem.quantity;
-    //             newQuantity = oldQuantity + 1;
-    //             return product;
-    //         }
-    //         return Product.findByPk(prodId);
-    //     }).then(product => {
-    //         return fetchedCart.addProduct(product, {
-    //             through: { quantity: newQuantity }
-    //         });
-    //     })
-    //     .then(() => {
-    //         res.redirect('/cart');
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //     });
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
@@ -108,9 +82,21 @@ exports.postCartDeleteProduct = (req, res, next) => {
 }
 
 exports.postOrder = (req, res, next) => {
-    let fetchedCart;
     req.user
-        .addOrder()
+        .populate('cart.items.productId')
+        .then(user => {
+            const products = user.cart.items.map(i => {
+                return { quantity: i.quantity, product: { ...i.productId._doc } };
+            });
+            const order = new Order({
+                user: {
+                    name: req.user.username,
+                    userId: req.user
+                },
+                products: products
+            });
+            return order.save();
+        })
         .then(result => {
             res.redirect('/orders');
         })
