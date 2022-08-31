394f4d57f7499b46cc9c861d1f5635f4f800d045
Fabio <cristovaofabio3@gmail.com>
2022-08-31 10:29:16 -0300

    feat: using session

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -20,6 +21,13 @@ const authRoutes = require('./router/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
 app.use((req, res, next) => {
     User.findById('6308bd809796725e508d28c9')
