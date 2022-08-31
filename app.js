41f6785e8406d145e1449795c6822ed2e9bf1527
Fabio <cristovaofabio3@gmail.com>
2022-08-31 10:56:49 -0300

    feat: storing sessions in MongoDB

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = `mongodb+srv://cristovaofabio:${process.env.MONGO_PASSWORD}@cluster0.sio0ccb.mongodb.net/shop`;
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -26,6 +33,7 @@ app.use(
         secret: 'my secret',
         resave: false,
         saveUninitialized: false,
+        store: store
     })
 );
 
@@ -44,7 +52,7 @@ app.use(authRoutes);
 
 app.use(errorController.pageNotFound);
 
-mongoose.connect(`mongodb+srv://cristovaofabio:${process.env.MONGO_PASSWORD}@cluster0.sio0ccb.mongodb.net/shop?retryWrites=true&w=majority`)
+mongoose.connect(MONGODB_URI)
     .then(result => {
         User.findOne().then(user => {
             if (!user) {
