a693b6d05dfa458231e1de538b11b62c193d98e6
Fabio <cristovaofabio3@gmail.com>
2022-09-03 11:01:58 -0300

    feat: encrypting password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -43,19 +45,24 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+
     User.findOne({ email: email })
         .then(userDoc => {
             if (userDoc) {
                 return res.redirect('/signup');
             }
-            const user = new User({
-                email: email,
-                password: password,
-                cart: { items: [] }
-            });
-            return user.save();
-        }).then(result => {
-            res.redirect('/login');
+            return bcrypt.hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        email: email,
+                        password: hashedPassword,
+                        cart: { items: [] }
+                    });
+                    return user.save();
+                })
+                .then(result => {
+                    res.redirect('/login');
+                });
         })
         .catch(err => {
             console.log(err);
