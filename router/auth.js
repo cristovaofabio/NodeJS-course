6b9b4243d0631120cc0bdf9b79d7efa31505ad47
Fabio <cristovaofabio3@gmail.com>
2022-09-09 11:12:57 -0300

    feat: password validator

--- router/auth.js
+++ router/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -11,7 +11,12 @@ router.get('/reset', authController.getReset);
 router.get('/reset/:token', authController.getNewPassword);
 
 router.post('/login', authController.postLogin);
-router.post('/signup', check('email').isEmail().withMessage('Please enter with a valid email.'), authController.postSignup);
+router.post('/signup',
+    [
+        check('email').isEmail().withMessage('Please enter with a valid email.'),
+        body('password', 'Invalid password! Insert only numbers and text and at least 6 characters').isLength({ min: 6 }).isAlphanumeric()
+    ],
+    authController.postSignup);
 router.post('/logout', authController.postLogout);
 router.post('/reset', authController.postReset);
 router.post('/new-password', authController.postNewPassword);
