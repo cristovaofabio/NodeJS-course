3607453fc8327cc0cf12c822bdb1538dbfd9e2e5
Fabio <cristovaofabio3@gmail.com>
2022-09-12 19:43:00 -0300

    feat: validating product editing

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const { validationResult } = require('express-validator');
+
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -6,6 +8,9 @@ exports.getAddProduct = (req, res, next) => {
         path: '/admin/add-product',
         productCSS: true,
         editing: false,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: []
     })
 }
 
@@ -14,6 +19,27 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Add product',
+            path: '/admin/edit-product',
+            editing: false,
+            productCSS: true,
+            activeAddProduct: true,
+            hasError: true,
+            product: {
+                title: title,
+                imageUrl: imageUrl,
+                price: price,
+                description: description,
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
+
     const product = new Product({
         title: title,
         price: price,
@@ -51,6 +77,9 @@ exports.getEditProduct = (req, res, next) => {
                 product: product,
                 productCSS: true,
                 activeAddProduct: true,
+                hasError: false,
+                errorMessage: null,
+                validationErrors: []
             });
         })
         .catch(err => console.log(err));
@@ -62,6 +91,28 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedPrice = req.body.price;
     const updatedDescription = req.body.description;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('admin/edit-product', {
+            pageTitle: 'Edit product',
+            path: '/admin/edit-product',
+            editing: true,
+            productCSS: true,
+            activeAddProduct: true,
+            hasError: true,
+            product: {
+                _id: prodId,
+                title: updatedTitle,
+                imageUrl: updatedImageUrl,
+                price: updatedPrice,
+                description: updatedDescription,
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
 
     Product.findById(prodId)
         .then(product => {
